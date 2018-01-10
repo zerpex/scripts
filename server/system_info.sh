a8c6594218a060d89133b8329df79df37ae7f505
zerpex <zerpex@gmail.com>
2018-01-10 14:10:33 +0100

    [fix] output.

--- server/system_info.sh
+++ server/system_info.sh
@@ -5,9 +5,9 @@
 echo -e "\e[31;43m***** HOSTNAME INFORMATION *****\e[0m"
 hostnamectl
 echo "       Server type: $(if [ -z "$(dmesg | grep Hypervisor)" ]; then echo "Physical"; else echo "Virtual"; fi)"
-echo "            Uptime: $(uptime | awk '{print $3}' | awk -F"," '{print $1" & "$2}')"
+echo "            Uptime: $(uptime | awk '{print $3}' | awk -F"," '{print $1}')"
 echo "      Load average: $(uptime | awk '{ for (i=8; i<=NF; i++) printf $i" " }')"
-echo "             Users: $(uptime | awk '{print $4}') ( $(for i in $(who -q | grep -v "#"); do echo -n "$i "; done))"
+echo "   Users connected: $(uptime | awk '{print $4}') ( $(for i in $(who -q | grep -v "#"); do echo -n "$i "; done))"
 echo ""
 
 
